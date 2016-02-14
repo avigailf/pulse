46d95d45bb82dff40c99d1939d9851b4638851e7
Avigail F <mazehle.ah@gmail.com>
2016-02-14 02:31:37 -0500

    Added click listener to the join button - starts ChooseGroup activity

--- app/src/main/java/com/example/pearlleff/pulse/ChooseGroup.java
+++ app/src/main/java/com/example/pearlleff/pulse/ChooseGroup.java
@@ -1,18 +1,41 @@
 package com.example.pearlleff.pulse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class ChooseGroup extends AppCompatActivity {
+    private Intent intent = getIntent();
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerViewAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_party);
+
+        recyclerView = (RecyclerView) findViewById(R.id.MissingPeopleList);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        //recyclerViewAdapter = new MissingPeopleListAdapter(myDataSet);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -20,8 +43,8 @@ public class ChooseGroup extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Adding missing person", Snackbar.LENGTH_LONG)
+                        .setAction("Undo", null).show();
             }
         });
     }
