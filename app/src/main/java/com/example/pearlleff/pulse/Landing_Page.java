005e34316f3caf8e68914158dff04a7079265140
nullisland <pearlleff@CEN-0034726.local>
2016-02-13 22:53:46 -0500

    Adding new files

---
+++ app/src/main/java/com/example/pearlleff/pulse/Landing_Page.java
@@ -0,0 +1,13 @@
+package com.example.pearlleff.pulse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Landing_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing__page);
+    }
+}
